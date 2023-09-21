51bc412528c23eeedf3a0e89185f09b3123de354
Kirill Kainov <kainov.dev@gmail.com>
2023-09-21 15:42:47 +0300

    rename security model class

--- src/main/scala/model/Security.scala
+++ src/main/scala/model/Security.scala
@@ -1,13 +1,13 @@
 package org.kainovk
 package model
 
+case class Security(secid: String,
+                    regnumber: String,
+                    name: String,
+                    emitentTitle: String)
+
 case class SecurityWithId(id: Option[Int],
                           secid: String,
                           regnumber: String,
                           name: String,
                           emitentTitle: String)
-
-case class Security(secid: String,
-                    regnumber: String,
-                    name: String,
-                    emitentTitle: String)
